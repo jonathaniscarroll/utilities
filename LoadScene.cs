70f27c19d3da5189a8762e1ca1fa21dab4c22598
Jonathan Edward Carroll <jonathaniscarroll@gmail.com>
2021-05-01 14:02:47 -0300

    Loadscene
    added async load op

--- LoadScene.cs
+++ LoadScene.cs
@@ -5,9 +5,54 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
-	public string SceneName;
+	public string SceneName{
+		get{return _sceneName;}
+		set{_sceneName = value;}
+	}
+	
+	[SerializeField]
+	private string _sceneName;
+	public bool Activate{
+		get;set;
+	}
+	public StringEvent OutputLoadProgress;
+	public StringEvent OnLoadComplete;
 	public void Load(){
 		SceneManager.LoadScene(SceneName);
+	}
+	
+	public void LoadAsync(){
+		StartCoroutine(AsyncCoroutine(SceneName));
+	}
+	
+	IEnumerator AsyncCoroutine(string scene){
+
+		//Begin to load the Scene you specify
+		AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(scene);
+		//Don't let the Scene activate until you allow it to
+		//asyncOperation.allowSceneActivation = false;
+		Debug.Log("Pro :" + asyncOperation.progress);
+		string text;
+		//When the load is still in progress, output the Text and progress bar
+		while (!asyncOperation.isDone)
+		{
+			//Output the current progress
+			text = "Loading progress: " + (asyncOperation.progress * 100) + "%";
+			OutputLoadProgress.Invoke(text);
+		
+			// Check if the load has finished
+			if (asyncOperation.progress >= 0.9f)
+			{
+				//Change the Text to show the Scene is ready
+				OnLoadComplete.Invoke(scene +" loaded");
+				//Wait to you press the space key to activate the Scene
+				if (Activate)
+					//Activate the Scene
+					asyncOperation.allowSceneActivation = true;
+			}
+
+			yield return new WaitForEndOfFrame();
+		}
 	}
 	
 	public void Exit(){
