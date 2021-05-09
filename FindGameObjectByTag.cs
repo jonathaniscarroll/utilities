61e027cf49bac27a6f0f04c8be7c4d03bd30805c
Jonathan Edward Carroll <jonathaniscarroll@gmail.com>
2021-05-09 15:45:26 -0300

    findgameobjectbytag added

---
+++ FindGameObjectByTag.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindGameObjectByTag : MonoBehaviour
+{
+	public GameObjectEvent OutputGameObject;
+	public void InputTag(string input){
+		GameObject output = GameObject.FindGameObjectWithTag(input);
+		OutputGameObject.Invoke(output);
+	}
+}
