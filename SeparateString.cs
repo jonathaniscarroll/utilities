edc6f5bdaed9960ca3cebf00c8038382585b4a01
Jonathan Edward Carroll <jonathaniscarroll@gmail.com>
2021-05-10 09:04:16 -0300

    added a number of utility scripts
    check if game object active
    output go tag
    separate string by char
    vector to string

---
+++ SeparateString.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeparateString : MonoBehaviour
+{
+	[SerializeField]
+	private string _parseString = " ";
+	public string ParseString{
+		get{
+			return _parseString;
+		} set{
+			_parseString = value;
+		}
+	}
+	public StringListEvent OutputSeparatedStrings;
+	
+	public void InputString(string input){
+		string[] str = input.Split(char.Parse(ParseString));
+		List<string> output = new List<string>(str);
+		OutputSeparatedStrings.Invoke(output);
+	}
+}
